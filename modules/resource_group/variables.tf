4af6f524d0de4498fed755b3df6eca665d125c00
bkrrajmali <bkrraj@gmail.com>
2026-09-13 12:24:40 +0530

    Update variables.tf

--- modules/resource_group/variables.tf
+++ modules/resource_group/variables.tf
@@ -1,10 +1,27 @@
 variable "resource_group_name" {
-  type = string
+  description = "Name of the resource group."
+  type        = string
 }
+
 variable "location" {
   type = string
 }
+
 variable "tags" {
   type    = map(string)
   default = {}
+}
+
+# --- added, both defaulted so existing calls need no change ---
+
+variable "create_lock" {
+  description = "Apply a CanNotDelete lock. Note it blocks your intentional destroy too: set false, apply, then destroy."
+  type        = bool
+  default     = false
+}
+
+variable "lock_notes" {
+  description = "Shown to whoever hits the lock, so make it useful."
+  type        = string
+  default     = "Locked to prevent accidental deletion."
 }
