afcae5427620cb442b792df76f957fe1a4261551
bkrrajmali <bkrraj@gmail.com>
2026-09-13 12:32:19 +0530

    Update outputs.tf

--- modules/acr/outputs.tf
+++ modules/acr/outputs.tf
@@ -2,3 +2,12 @@ output "acr_id" {
   description = "The resource ID of the Azure Container Registry."
   value       = azurerm_container_registry.acr.id
 }
+
+output "name" {
+  value = azurerm_container_registry.acr.name
+}
+
+output "login_server" {
+  description = "Registry hostname. Prefix your image tags with this."
+  value       = azurerm_container_registry.acr.login_server
+}
