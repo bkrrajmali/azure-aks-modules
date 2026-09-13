c50ac8345cd33e6a5be2dbda97f9566fab00d3f3
bkrrajmali <bkrraj@gmail.com>
2026-09-13 12:36:09 +0530

    Update main.tf

--- modules/postgres-database/main.tf
+++ modules/postgres-database/main.tf
@@ -14,4 +14,10 @@ resource "azurerm_postgresql_flexible_server_database" "this" {
   server_id = var.server_id
   collation = var.collation
   charset   = var.charset
+
+  lifecycle {
+    # Dropping a database drops the data. This also blocks `terraform destroy`
+    # on the whole config until you remove it -- deliberate friction.
+    prevent_destroy = true
+  }
 }
